424381b3c9a4a167a94bf14eb5a0adcf273f8438
irazramin <irazramin@gmail.com>
2022-05-21 21:47:24 +0600

    inital commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClientProvider } from 'react-query';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </QueryClientProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#6a50b2',
+
+          secondary: '#8c84f9',
+
+          accent: '#83c914',
+
+          neutral: '#26202C',
+
+          'base-100': '#26284A',
+
+          info: '#9CC9DE',
+
+          success: '#30DFCA',
+
+          warning: '#F9BA4D',
+
+          error: '#E53864',
+        },
+      },
+    ],
+  },
+  plugins: [require('daisyui')],
+};
