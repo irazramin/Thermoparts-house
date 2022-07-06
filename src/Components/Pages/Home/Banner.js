fb727fea7d0c9e69651fab2d6e3108b99cf89bd1
irazramin <irazramin@gmail.com>
2022-07-06 17:11:11 +0600

    imgs update

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -5,7 +5,7 @@ const Banner = () => {
     <div className='carousel w-full h-[70vh]'>
       <div id='slide1' className='carousel-item relative w-full'>
         <img
-          src='https://images.unsplash.com/photo-1647427060118-4911c9821b82?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470'
+          src='https://images.pexels.com/photos/7484157/pexels-photo-7484157.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
           className='object-cover w-full'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
@@ -19,7 +19,7 @@ const Banner = () => {
       </div>
       <div id='slide2' className='carousel-item relative w-full'>
         <img
-          src='https://images.unsplash.com/photo-1606206873764-fd15e242df52?crop=entropy&cs=tinysrgb&fm=jpg&ixlib=rb-1.2.1&q=80&raw_url=true&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070'
+          src='https://images.pexels.com/photos/5759611/pexels-photo-5759611.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
           className='object-cover w-full'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
@@ -33,7 +33,7 @@ const Banner = () => {
       </div>
       <div id='slide3' className='carousel-item relative w-full'>
         <img
-          src='https://images.unsplash.com/photo-1589792923962-537704632910?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170'
+          src='https://images.pexels.com/photos/2760241/pexels-photo-2760241.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2'
           className='object-cover w-full'
         />
         <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
